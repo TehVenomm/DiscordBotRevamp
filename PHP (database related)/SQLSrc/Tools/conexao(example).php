5eb278978977c0f754a81c832cff0a0aadaa62b5
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-11-03 16:38:34 -0300

    Commit

--- PHP (database related)/SQLSrc/Tools/conexao(example).php
+++ PHP (database related)/SQLSrc/Tools/conexao(example).php
@@ -5,11 +5,11 @@
 		{
 			header('Content-Type: text/html; charset=utf-8');
 		}
-		$conex = new PDO("mysql:host=85.10.205.173:3306;dbname=karyu_db;charset=utf8","*****","****");
+		$conex = new PDO("mysql:host=hostname;dbname=database_name;charset=utf8","databaseusername","databasepassword");
 		$conex ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$conex -> query("SET NAMES 'utf8'");
 		$conex -> query("SET CHARACTER SET 'utf8'");
-		set_time_limit(0);
+		set_time_limit(900);
 	}
 	catch(PDOexception $e)
 	{
