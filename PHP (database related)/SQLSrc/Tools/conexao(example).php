1a9fa14ddc3f4dcbaed8375827045bbd6ac87f4e
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-12-15 18:38:17 -0200

    Fixing character encoding issues

--- PHP (database related)/SQLSrc/Tools/conexao(example).php
+++ PHP (database related)/SQLSrc/Tools/conexao(example).php
@@ -5,7 +5,7 @@
 		{
 			header('Content-Type: text/html; charset=utf-8');
 		}
-		$conex = new PDO("mysql:host=hostname;dbname=database_name;charset=utf8","databaseusername","databasepassword");
+		$conex = new PDO("mysql:host=hostname;dbname=database_name;charset=utf8","databaseusername","databasepassword", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 		$conex ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$conex -> query("SET NAMES 'utf8'");
 		$conex -> query("SET CHARACTER SET 'utf8'");
