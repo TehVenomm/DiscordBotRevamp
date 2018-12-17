f32a04ad3bed1716a89c6023391ce1b68ffbf818
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-12-16 23:28:05 -0200

    Armour optimization
    might cause bugs, needs testing

--- PHP (database related)/SQLSrc/GeneralScripts/linkArmourStash.php
+++ PHP (database related)/SQLSrc/GeneralScripts/linkArmourStash.php
@@ -5,8 +5,42 @@
 
     $spreadsheet_data = grabCSVData($linkArmour);
 
-    $skip2rowscounter  = 0; 
-    $errorCounter      = 0; 
+    $skip2rowscounter  		= 0; 
+    $errorCounter      		= 0; 
+	$armorQueryArray 		= [];
+	$idBehemothArray 		= [];
+	$idBehemothArmorArray 	= [];
+
+
+	$sql = "SELECT idBehemoth, Name FROM behemothtable";
+	try
+	{
+		$setupQuery = $conex -> query($sql);
+
+	} catch (Exception $setupQuery) 
+	{
+		echo 'Caught exception: ',  $setupQuery->getMessage(), "</br>";
+	}
+	foreach($setupQuery as $x)
+	{
+		$idBehemothArray[$x['Name']] = $x["idBehemoth"];
+	}
+	
+	
+	$sql = "SELECT IdBehemoth_ArmourTable FROM armourtable";
+	try
+	{
+		$setupQuery = $conex -> query($sql);
+
+	} catch (Exception $setupQuery) 
+	{
+		echo 'Caught exception: ',  $setupQuery->getMessage(), "</br>";
+	}
+	foreach($setupQuery as $x)
+	{
+		$idBehemothArmorArray[] = $x["IdBehemoth_ArmourTable"];
+	}
+
 
     foreach($spreadsheet_data as $rowcount)
     {
@@ -31,78 +65,70 @@
             $eDefLegsCSV        = $rowcount[17];
             $pAtkLegsCSV        = $rowcount[18];
 
-            $abilityCSV         = $rowcount[20];
-            $obsCSV             = filterNotObs($rowcount[21]);
+            $abilityCSV         = str_replace("'", "\'", $rowcount[20]);
+            $obsCSV             = str_replace("'", "\'", filterNotObs($rowcount[21]));
 
-            //get existing behemoth's id
-            $sql = "SELECT idBehemoth FROM behemothtable WHERE behemothtable.Name = ?";
-            $behemothQuery = $conex -> prepare($sql);
-
-            try
-            {
-                $behemothQuery -> execute(array($behemothNameCSV));
-
-            } catch (Exception $behemothQuery) 
-            {
-                echo 'Caught exception: ',  $behemothQuery->getMessage(), "</br>";
-            }
-            foreach($behemothQuery as $x)
-            {
-                $idBehemothQuery = $x["idBehemoth"];
-            }
-
-            $qty = $behemothQuery->rowCount();
-            if (!empty($abilityCSV))
-            {
-                if($qty != 0)
-                {
-                    $sql = "INSERT INTO `armourtable` VALUES ('', ?, ?, 0, ?, 0, ?, ?, 1, ?)"; //HELMET
-                    $armoryHelmet = $conex -> prepare($sql);
-                    try
-                    {
-                        
-                        $armoryHelmet -> execute(array($defElementCSV, $hpHelmetCSV, $eDefHelmetCSV, $abilityCSV, $obsCSV, $idBehemothQuery));
-                    } catch (Exception $armoryHelmet) 
-                    {
-                        echo 'Caught exception: ',  $armoryHelmet->getMessage(), "</br>";
-                    }
-
-                    $sql = "INSERT INTO `armourtable` VALUES ('', ?, 0, ?, ?, 0, ?, ?, 2, ?)"; //CHEST
-                    $armoryChest = $conex -> prepare($sql);
-                    try
-                    {
-                        $armoryChest -> execute(array($defElementCSV, $pDefChestCSV, $eDefChestCSV, $abilityCSV, $obsCSV, $idBehemothQuery));
-                    } catch (Exception $armoryChest) {
-                        echo 'Caught exception: ',  $armoryChest->getMessage(), "</br>";
-                    }
-
-                    $sql = "INSERT INTO `armourtable` VALUES ('', ?, ?, ?, ?, ?, ?, ?, 3, ?)"; //GLOVES
-                    $armoryGloves = $conex -> prepare($sql);
-                    try{
-                        $armoryGloves -> execute(array($defElementCSV, $hpGlovesCSV, $pDefGlovesCSV, $eDefGlovesCSV, $pAtkGlovesCSV, $abilityCSV, $obsCSV, $idBehemothQuery));
-                    } catch (Exception $armoryGloves) {
-                        echo 'Caught exception: ',  $armoryGloves->getMessage(), "</br>";
-                    }
-
-                    $sql = "INSERT INTO `armourtable` VALUES ('', ?, ?, ?, ?, ?, ?, ?, 4, ?)"; //LEGS
-                    $armoryLegs = $conex -> prepare($sql);
-                    try{
-                        $armoryLegs -> execute(array($defElementCSV, $hpLegsCSV, $pDefLegsCSV, $eDefLegsCSV, $pAtkLegsCSV, $abilityCSV, $obsCSV, $idBehemothQuery));
-                    } catch (Exception $armoryLegs) {
-                        echo 'Caught exception: ',  $armoryLegs->getMessage(), "</br>";
-                    }
-
-                } else 
-                {
-                    $errorCounter++;
-                    echo "behemoth ->".$behemothNameCSV."<- not found thus registering failed.<br>";
-                }
-            }
+			if (!empty($abilityCSV))
+			{
+				if(isset($idBehemothArray[$behemothNameCSV]))
+				{
+					$idBehemothQuery = $idBehemothArray[$behemothNameCSV];
+					if(!in_array($idBehemothQuery, $idBehemothArmorArray)){
+						$armorQueryArray[] = " ( '".$defElementCSV."', '".$hpHelmetCSV."', 0, '".$eDefHelmetCSV."', 0, '".$abilityCSV."', '".$obsCSV."', 1, ".$idBehemothQuery." ) ";
+						$armorQueryArray[] = " ( '".$defElementCSV."', 0, '".$pDefChestCSV."', '".$eDefChestCSV."', 0, '".$abilityCSV."', '".$obsCSV."', 2, ".$idBehemothQuery." ) ";
+						$armorQueryArray[] = " ( '".$defElementCSV."', '".$hpGlovesCSV."', '".$pDefGlovesCSV."', '".$eDefGlovesCSV."', '".$pAtkGlovesCSV."', '".$abilityCSV."', '".$obsCSV."', 3, ".$idBehemothQuery." ) ";
+						$armorQueryArray[] = " ( '".$defElementCSV."', '".$hpLegsCSV."', '".$pDefLegsCSV."', '".$eDefLegsCSV."', '".$pAtkLegsCSV."', '".$abilityCSV."', '".$obsCSV."', 4, ".$idBehemothQuery." ) ";
+					} else {
+						$errorCounter++;
+						echo "behemoth ->".$behemothNameCSV."<- already has armour pieces registered<br>";
+					}
+				} else 
+				{
+					$errorCounter++;
+					echo "behemoth ->".$behemothNameCSV."<- not found thus registering failed.<br>";
+				}
+			}
+			
         }
 
         $skip2rowscounter++;
 
     }
+	// this thing can be made into a genericFunction, its gonna be applied like 4x over the source code so shorten this bitch
+	
+	$sql = "INSERT INTO `armourtable` (`DefElement`, `HpValue`, `PhysDef`, `ElemDef`, `PhysAttack`, `Ability`, `Obs`, `IdArmourtype_ArmourTable`, `IdBehemoth_ArmourTable`) VALUES ";
+	$i = 0;
+		
+	if (is_array($armorQueryArray)) {
+		foreach ($armorQueryArray as $row){
+			if( !next( $armorQueryArray ) ) {
+				$sql .= $row."; ";
+			} else {
+				$sql .= $row.", ";
+			}
+		}
+	} elseif (!empty($armorQueryArray)){
+		$sql .= $armorQueryArray."; ";
+	}
+	
+	if (!empty($armorQueryArray)){	
+		try
+		{
+			$insertArmourQuery = $conex -> query($sql);
+		} 
+		catch (Exception $insertArmourQuery) 
+		{
+			echo 'Caught exception: ',  $insertArmourQuery->getMessage(), "</br>";
+			$conex = NULL;
+			return false;
+		}
+	} else {
+		echo "Nothing to insert.";
+		return false;
+	}
+
+	$conex = NULL;
+	
     if ($errorCounter == 0)
         echo "Success.</br>";
     else   
